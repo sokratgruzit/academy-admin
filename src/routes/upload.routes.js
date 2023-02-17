9c57dc0d3736db80d1bc6aacc68cbd99fc74b47d
gogaokradze <deathofsuper@gmail.com>
2023-02-17 15:31:14 +0400

    fix: iamge upload limit backend

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -2,7 +2,10 @@ const { Router } = require("express");
 const router = Router();
 const Multer = require("multer");
 
-const multer = Multer({ storage: Multer.memoryStorage() });
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: { fileSize: 2 * 1024 * 1024 },
+});
 
 const UploadController = require("../middlewares/upload.middleware");
 const AuthMiddleware = require("../middlewares/auth.middleware");
